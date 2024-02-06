22579509a372687cebf9a1b143eb094e395aba19
Matias Torres <mtri21019@gmail.com>
2024-02-06 12:42:54 -0300

    creación del carrito de compras

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,16 +2,18 @@ import { Component, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from './../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  cart = signal<Product[]>([]);
 
   constructor() {
     const initProducts: Product[] = [
@@ -36,12 +38,32 @@ export class ListComponent {
         image: 'https://picsum.photos/640/640?r=24',
         creationAt: new Date().toISOString(),
       },
+      {
+        id: Date.now(),
+        title: 'Pro 4',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=25',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 5',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=26',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 6',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=27',
+        creationAt: new Date().toISOString(),
+      },
     ];
     this.products.set(initProducts);
   }
 
-  fromChild(event: string) {
-    console.log('estamos en el padre');
-    console.log(event);
+  addToCart(product: Product) {
+    this.cart.update((prevState) => [...prevState, product]);
   }
 }
---
+++ src/app/domains/shared/components/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  hideSideMenu = signal(true);
+  @Input({ required: true }) cart: Product[] = [];
+  total = signal(0);
+
+  toogleSideMenu() {
+    this.hideSideMenu.update((prevstate) => !prevstate);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const cart = changes['cart'];
+    if(cart) {
+      this.total.set(this.calcTotal())
+    }
+  }
+
+  calcTotal() {
+    return this.cart.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }
+}

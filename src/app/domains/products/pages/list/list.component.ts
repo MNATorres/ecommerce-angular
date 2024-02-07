e8913ef57644f630ac6ae5c7b77c4989d922831e
matias.torres <mtri21019@gmail.com>
2024-02-07 11:04:58 -0300

    conectando a una api, sse crea nuevo servicio

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { Product } from '../../../shared/models/product.model';
 import { HeaderComponent } from './../../../shared/components/header/header.component';
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-list',
@@ -15,57 +17,20 @@ import { CartService } from '../../../shared/services/cart.service';
 export class ListComponent {
   products = signal<Product[]>([]);
   private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
-
-  constructor() {
-    const initProducts: Product[] = [
-      {
-        id: Date.now(),
-        title: 'Pro 1',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=23',
-        creationAt: new Date().toISOString(),
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
       },
-      {
-        id: Date.now(),
-        title: 'Pro 2',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=12',
-        creationAt: new Date().toISOString(),
+      error: () => {
+        console.log(error);
       },
-      {
-        id: Date.now(),
-        title: 'Pro 3',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=24',
-        creationAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 4',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=25',
-        creationAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 5',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=26',
-        creationAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 6',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=27',
-        creationAt: new Date().toISOString(),
-      },
-    ];
-    this.products.set(initProducts);
+    });
   }
 
   addToCart(product: Product) {
-    this.cartService.addToCart(product)
+    this.cartService.addToCart(product);
   }
 }

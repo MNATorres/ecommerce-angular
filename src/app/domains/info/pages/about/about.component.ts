21164b845f44e42e8a26093596f818b3a5d75a24
matias.torres <mtri21019@gmail.com>
2024-02-07 12:10:24 -0300

    uso de directivas, manipulación directa del DOM

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CounterComponent } from '../../../shared/components/counter/counter.component';
+import { CounterComponent } from '@shared/components/counter/counter.component';
 import { WaveAudioComponent } from './../../components/wave-audio/wave-audio.component';
+import { HighlightDirective } from '@shared/directives/highlight.directive';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  imports: [CommonModule, CounterComponent, WaveAudioComponent],
+  imports: [CommonModule, CounterComponent, WaveAudioComponent, HighlightDirective],
 })
 export class AboutComponent {
   duration = signal(1000);
